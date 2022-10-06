a7e89774b6c198dfea019ecf6c08e59190aefb8d
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-10-06 01:19:05 -0400

    changed settings for templates

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -67,6 +67,11 @@ TEMPLATES = [
         },
     },
 ]
+
+TEMPLATE_DIRS = (
+    os.path.join(SETTINGS_PATH, 'templates'),
+)
+
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
@@ -124,5 +129,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['enigmatic-cliffs-62735.herokuapp.com']
+
+
 
 
