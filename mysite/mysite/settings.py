10ca716c730958f63ff9d9b97cf1c1d3679e6b17
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-10-11 05:23:57 -0400

    edited middleware

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
