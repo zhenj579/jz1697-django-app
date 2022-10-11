6d155c6e612bccfb927d08e98bb858497e27602a
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-10-11 04:57:20 -0400

    fixed template bug

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -20,12 +20,6 @@ class IndexView(generic.ListView):
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
@@ -51,7 +45,8 @@ def vote(request, question_id):
 
 
 class DetailView(generic.DetailView):
-    ...
+    model = Question
+    template_name = 'polls/detail.html'
 
     def get_queryset(self):
         """
