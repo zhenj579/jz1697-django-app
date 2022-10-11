eaef933d99dbabead8b856cf18473aea015ec0e4
John Zhen <john.zhen24@myhunter.cuny.edu>
2022-10-11 05:12:35 -0400

    enabled whitenoise

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
